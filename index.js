5d5f2b195d186686e2288e0109ae17b0c68780d1
walid885 <bettaieb88walid@gmail.com>
2023-03-03 20:52:31 +0100

    starting the server using nodemon

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const Logger = require('./Logger')
+/*const Logger = require('./Logger')
 const logger = new Logger();
 logger.on('message',(data) => console.log('Called listner ', data)); 
-logger.log("hello world ")
+logger.log("hello world ") */ 
+
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+
+const server = http.createServer((req,res)=>{
+if ( req.url === '/'){
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end('<h1> Home mdafakaaaa </h1>')
+}
+
+}); 
+const port = process.env.port || 5000
+server.listen(port,() => console.log(`Server is running on port ${port}`))
