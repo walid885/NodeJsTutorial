a8827ca4d11af8a85580cd54a243b1b8077a5c27
walid885 <bettaieb88walid@gmail.com>
2023-03-03 21:28:08 +0100

    using data in JSON format

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ if ( req.url === '/'){
     })
 
 }
-if ( req.url === '/about'){
-    fs.readFile(path.join(__dirname,'public','about.html') , (err, content) =>{
-        res.writeHead(200, {'content-type':'text/html'});
-        if (err) throw err ;
-        res.end(content);
+if ( req.url === '/api/users'){
+const users = [
+    {name: 'Bob Smith',age:40},
+    {name: 'John DOe', age:30}
 
-    })
-
+];
+res.writeHead(200,{'Content-Type': 'application/json' })
+res.end(JSON.stringify(users));
 }
 
 }); 
